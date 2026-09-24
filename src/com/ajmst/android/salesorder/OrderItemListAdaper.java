d7cb44b257f1bdab2eb435732e72f2bc585a62b4
caijun <caijun2@supcon.com>
2026-09-24 14:07:07 +0800

    Align-sales-order-quantity-and-action-buttons

--- src/com/ajmst/android/salesorder/OrderItemListAdaper.java
+++ src/com/ajmst/android/salesorder/OrderItemListAdaper.java
@@ -1,5 +1,6 @@
 package com.ajmst.android.salesorder;
 
+import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.List;
 
@@ -9,6 +10,11 @@ import com.ajmst.android.service.SalesOrderService;
 import com.ajmst.android.service.SpkfkService;
 
 import android.app.Activity;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -75,9 +81,9 @@ public class OrderItemListAdaper extends BaseAdapter{
 		TextView textViewSeq = (TextView)convertView.findViewById(R.id.textViewSeq);
 		textViewSeq.setText("" + (position + 1));
 		TextView tvSpmch = (TextView)convertView.findViewById(R.id.tvSpmch);
-		tvSpmch.setText(orderItem.getSpmch());
 		TextView tvShpgg = (TextView)convertView.findViewById(R.id.tvShpgg);
 		tvShpgg.setText(orderItem.getShpgg());
+		tvShpgg.setVisibility(TextUtils.isEmpty(orderItem.getShpgg()) ? View.GONE : View.VISIBLE);
 /*		TextView tvCabinetNo = (TextView)convertView.findViewById(R.id.tvCabinetNo);
 		tvCabinetNo.setText(spkfk.getGh());*/
 		TextView tvLshj = (TextView)convertView.findViewById(R.id.tvLshj);
@@ -89,16 +95,23 @@ public class OrderItemListAdaper extends BaseAdapter{
 		TextView tvItemAmount = (TextView)convertView.findViewById(R.id.tvItemAmount);
 		tvItemAmount.setText("¥" + amountFormat.format(SalesOrderService.getItemAmount(orderItem)));
 		
-		TextView tvShl = (TextView)convertView.findViewById(R.id.tvShl);
-		String quantity = orderItem.getShl() % 1 == 0
-				? String.valueOf(orderItem.getShl().intValue()) : orderItem.getShl().toString();
+		String quantity = BigDecimal.valueOf(orderItem.getShl()).stripTrailingZeros().toPlainString();
 		String unit = isChineseMedicine ? "g" : orderItem.getDw();
 		if (unit == null) {
 			unit = "";
 		}
-		tvShl.setText(quantity + unit + "  ✎");
-		tvShl.setContentDescription("修改" + orderItem.getSpmch() + "的数量，当前" + quantity + unit);
-		tvShl.setOnClickListener(new OnClickListener() {
+		String name = orderItem.getSpmch() == null ? "" : orderItem.getSpmch();
+		SpannableString label = new SpannableString(name + " " + quantity + unit);
+		int quantityStart = name.length() + 1;
+		label.setSpan(new ForegroundColorSpan(activity.getResources().getColor(R.color.ui_accent_dark)),
+				quantityStart, label.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+		label.setSpan(new RelativeSizeSpan(0.75f),
+				quantityStart, label.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+		tvSpmch.setText(label);
+		tvSpmch.setContentDescription(name + "，数量" + quantity + unit);
+		Button editButton = (Button) convertView.findViewById(R.id.btnEditQuantity);
+		editButton.setContentDescription("修改" + name + "的数量，当前" + quantity + unit);
+		editButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				((SalesOrderActivity) activity).editOrderItemQuantity(position);
@@ -108,7 +121,7 @@ public class OrderItemListAdaper extends BaseAdapter{
 		convertView.setTag(orderItem);
 		View foreground = convertView.findViewById(R.id.orderRowForeground);
 		boolean isOpen = position == openPosition;
-		foreground.setTranslationX(isOpen ? -72 * activity.getResources().getDisplayMetrics().density : 0);
+		foreground.setTranslationX(isOpen ? -52 * activity.getResources().getDisplayMetrics().density : 0);
 		Button deleteButton = (Button) convertView.findViewById(R.id.btnDeleteItem);
 		deleteButton.setVisibility(isOpen ? View.VISIBLE : View.INVISIBLE);
 		deleteButton.setOnClickListener(new OnClickListener() {
